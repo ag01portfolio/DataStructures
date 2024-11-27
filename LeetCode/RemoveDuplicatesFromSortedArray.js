0112b93824f9f85f724d5b49439a141ec9ae4241
Abhi - ROG <ag01portfolio@gmail.com>
2024-11-27 09:41:03 +0530

    removeDuplicates sol

--- LeetCode/RemoveDuplicatesFromSortedArray.js
+++ LeetCode/RemoveDuplicatesFromSortedArray.js
@@ -1,10 +1,32 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array/?envType=problem-list-v2&envId=array
 
-let arr = [1, 2, 3, 1];
+let arr = [1, 2, 2, 3];
+// let arr2 = [];
 
-function removeDuplicates(nums) {
-  arr2 = [];
-  for (i = 0; i < nums.length; i++) {}
-}
+// function removeDuplicates(nums) {
+//   for (i = 0; i < nums.length; i++) {
+//     if (nums[i] === nums[i + 1]) {
+//       arr2.push("_");
+//       console.log("if True ");
+//     } else {
+//       arr2.push(nums[i]);
+//       console.log("if false ");
+//     }
+//   }
+// }
 
-removeDuplicates(arr);
+// r = removeDuplicates(arr);
+// console.log("arr2: ", arr2);
+// console.log("Res: ", r);
+
+var removeDuplicates = function (nums) {
+  let newNumArr = [];
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+    } else {
+      newNumArr.push(nums[i]);
+    }
+  }
+
+  return newNumArr;
+};
