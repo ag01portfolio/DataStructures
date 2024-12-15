9c27146374cfce7ff448f502cd5f3641b78bea22
Abhi - ROG <ag01portfolio@gmail.com>
2024-12-15 17:18:01 +0530

    search-insert-position

---
+++ LeetCode/Adding-spaces-to-a-string.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/adding-spaces-to-a-string/description/?envType=daily-question&envId=2024-12-03
+
+function space(s, sp) {
+  let a = s;
+  for (i = 0; i < s.length; i++) {
+    if (s[i] === sp) {
+    }
+  }
+  return a;
+}
+
+space("Abhi", 2);
